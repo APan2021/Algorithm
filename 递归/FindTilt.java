3311300869d2b1fcc1fbb7e015e298043bd5faeb
APan2021 <83541492+APan2021@users.noreply.github.com>
2021-05-10 20:06:20 +0800

    Create FindTilt.java

---
+++ 递归/FindTilt.java
@@ -0,0 +1,18 @@
+
+class FindTilt {
+    
+    int tilt = 0;
+    public int findTilt(TreeNode root) {
+        find(root);
+        return tilt;
+    }
+
+    public int find(TreeNode root) {
+        if (root == null) return 0;
+        int left = find(root.left);
+        int right = find(root.right);
+        tilt += Math.abs(left - right);
+        return left + right + root.val;
+    }
+
+}
