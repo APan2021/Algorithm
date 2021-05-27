34f4f1f092efe1462ea0fef0146e339c40bf9715
APan2021 <83541492+APan2021@users.noreply.github.com>
2021-05-27 14:43:42 +0800

    Create MaxDepth.java
    给定一个 N 叉树，找到其最大深度。
    
    最大深度是指从根节点到最远叶子节点的最长路径上的节点总数。
    
    N 叉树输入按层序遍历序列化表示，每组子节点由空值分隔

---
+++ BreadthFirstSearch/MaxDepth.java
@@ -0,0 +1,11 @@
+class MaxDepth {
+    public int maxDepth(Node root) {
+        if (root == null) return 0;
+        if (root.children.isEmpty()) return 1;
+        int max = 0;
+        for (Node child : root.children) {
+            max = Math.max(max, maxDepth(child));
+        }
+        return max + 1;
+    }
+}
